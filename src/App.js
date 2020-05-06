4746eda7ded077114816a2e421d3ea31afb3fe71
basalcode <basalcode@gmail.com>
2020-05-06 23:51:01 +0900

    add : Set HTML layout of the main page
    What's New?
    * Layout was expressed with HTML elements.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,29 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, BasalCodeStudio!
+        <header>
+          <a>BasalCodeSutio</a>
+          <nav>
+            <a>Blog</a>
+            <a>Works</a>
+            <a>Links</a>
+          </nav>
+          <p>I Do Web, So You Don't Have To Worry About It.</p>
+        </header>
+        <section>
+          <article>
+            <h1>Intro</h1>
+            <p>About Intro...</p>
+          </article>
+          <article>
+            <h1>Value</h1>
+            <p>About Value</p>
+          </article>
+          <article>
+            <h1>Contact</h1>
+            <p>About Contact</p>
+          </article>
+        </section>
       </div>
     );
   }
