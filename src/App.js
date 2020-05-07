69ee45561a43d8552a3851add1b6669d662194d7
basalcode <basalcode@gmail.com>
2020-05-07 23:41:02 +0900

    add : Create component which contains main page
    What's New?
    * Main page HTML was encapsulated with a component.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import MainPage from './components/MainPage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
-          <a>BasalCodeSutio</a>
-          <nav>
-            <a>Blog</a>
-            <a>Works</a>
-            <a>Links</a>
-          </nav>
-          <p>I Do Web, So You Don't Have To Worry About It.</p>
-        </header>
-        <section>
-          <article>
-            <h1>Intro</h1>
-            <p>About Intro...</p>
-          </article>
-          <article>
-            <h1>Value</h1>
-            <p>About Value</p>
-          </article>
-          <article>
-            <h1>Contact</h1>
-            <p>About Contact</p>
-          </article>
-        </section>
+        <MainPage></MainPage>
       </div>
     );
   }
