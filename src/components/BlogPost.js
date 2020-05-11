10611dab7cbbe58246b3adaaaedbf68f64cacf30
basalcode <basalcode@gmail.com>
2020-05-11 23:58:11 +0900

    add : Create component which contains BlogPost page
    What's New?
    * 'BlogPost.js' which contains HTML information about component was created
      and set.

---
+++ src/components/BlogPost.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+class BlogPost extends Component {
+  render() {
+    return (
+        <div>
+            <header>
+                <a>logo</a>
+                <nav>navigation</nav>
+            </header>
+            <main>
+                <p>Introduction of this page</p>
+                <article>
+                    <img />
+                    <h1>Content 1</h1>
+                    <p>Preview about this content...</p>
+                </article>
+                <article>
+                    <img />
+                    <h1>Content 2</h1>
+                    <p>Preview about this content...</p>
+                </article>
+            </main>
+        </div>
+    );
+  }
+}
+export default BlogPost;
