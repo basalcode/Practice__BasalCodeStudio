aa7e422dc77688cc829adb0197738a3fbf7f004b
basalcode <basalcode@gmail.com>
2020-05-12 23:29:29 +0900

    add : Create component which contains Enterance page
    What's New?
    * 'Enterance.js' which contains HTML information about component was created
      and set.

---
+++ src/components/Enterance.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Enterance extends Component {
+  render() {
+    return (
+        <div>
+            <main>
+                <div>Basal Code</div>
+                <img />
+            </main>
+        </div>
+    );
+  }
+}
+
+export default Enterance;
