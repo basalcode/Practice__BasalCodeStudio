bf98624c0bea07b603f95fbf200f76976962caf9
basalcode <basalcode@gmail.com>
2020-05-21 23:54:36 +0900

    add : Start to make detailed layout on lobby page
    What's New?
    * CSS stylesheets for lobby page have been made.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import './component/page/main/Enterance.css';
 import './component/template/basic.css'
+import './component/page/main/Enterance.css';
+import './component/page/main/Lobby.css';
 import Enterance from './component/page/main/Enterance';
+import Lobby from './component/page/main/Lobby';
 
 class App extends Component {
   render() {
     return (
       <div id="App" className="basic--max-viewport">
-        <Enterance></Enterance>
+        {/* <Enterance></Enterance> */}
+        <Lobby></Lobby>
       </div>
     );
   }
--- src/component/page/main/Lobby.js
+++ src/component/page/main/Lobby.js
@@ -4,25 +4,28 @@ class Lobby extends Component {
     render() {
         return (
             <div>
-                <header>
-                    <a>BasalCodeSutio</a>
-                    <nav>
-                        <a>Blog</a>
-                        <a>Works</a>
-                        <a>Links</a>
+                <header className="lobby__header">
+                    <a className="lobby__header--logo">BasalCodeSutio</a>
+                    <nav className="lobby__header--nav">
+                        <li><a>Blog</a></li>
+                        <li><a>Works</a></li>
+                        <li><a>Links</a></li>
                     </nav>
-                    <p>I Do Web, So You Don't Have To Worry About It.</p>
                 </header>
-                <main role="main">
-                    <article>
+                <section className="lobby__preface">
+                    <div className="lobby__preface--image"></div>
+                    <p className="lobby__preface--text">I Do Web, So You Don't Have To Worry About It.</p>
+                </section>
+                <main role="main" id="lobby__main">
+                    <article className="lobby__content">
                         <h1>Intro</h1>
                         <p>About Intro...</p>
                     </article>
-                    <article>
+                    <article className="lobby__content">
                         <h1>Value</h1>
                         <p>About Value</p>
                     </article>
-                    <article>
+                    <article className="lobby__content">
                         <h1>Contact</h1>
                         <p>About Contact</p>
                     </article>
